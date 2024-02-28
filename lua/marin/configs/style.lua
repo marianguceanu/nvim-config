de3e42ec22117c26c8e0eb104b2050db67369e1f
Marian Guceanu <marian.guceanu@yahoo.com>
2024-02-28 15:00:19 +0200

    tweaks

--- lua/marin/configs/style.lua
+++ lua/marin/configs/style.lua
@@ -1,2 +1,28 @@
 -- Tokyonight theme, we also like
-vim.cmd([[colorscheme tokyonight-night]])
+-- vim.cmd([[colorscheme tokyonight-night]])
+
+-- Lua
+require("onedark").setup({
+	-- Main options --
+	style = "deep", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+	transparent = false, -- Show/hide background
+	term_colors = true, -- Change terminal color as per the selected theme style
+	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+
+	code_style = {
+		comments = "italic",
+		keywords = "none",
+		functions = "none",
+		strings = "none",
+		variables = "none",
+	},
+	-- Plugins Config --
+	diagnostics = {
+		darker = true, -- darker colors for diagnostic
+		undercurl = true, -- use undercurl instead of underline for diagnostics
+		background = true, -- use background color for virtual text
+	},
+})
+
+require("onedark").load()
