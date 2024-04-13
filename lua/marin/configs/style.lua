805193073cd0e59091f0a246b40ebda55cae09f7
Marian Guceanu <marian.guceanu@yahoo.com>
2024-04-13 16:31:12 +0300

    added tokyonight and made keymaps for switching between light and dark mode

--- lua/marin/configs/style.lua
+++ lua/marin/configs/style.lua
@@ -1,24 +1,35 @@
-require("onedark").setup({
-	-- Main options --
-	style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
-	transparent = false, -- Show/hide background
-	term_colors = true, -- Change terminal color as per the selected theme style
-	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
-	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+-- require("onedark").setup({
+-- 	-- Main options --
+-- 	style = "darker", -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+-- 	transparent = false, -- Show/hide background
+-- 	term_colors = true, -- Change terminal color as per the selected theme style
+-- 	ending_tildes = false, -- Show the end-of-buffer tildes. By default they are hidden
+-- 	cmp_itemkind_reverse = false, -- reverse item kind highlights in cmp menu
+--
+-- 	code_style = {
+-- 		comments = "italic",
+-- 		keywords = "italic",
+-- 		functions = "bold",
+-- 		strings = "none",
+-- 		variables = "none",
+-- 	},
+-- 	-- Plugins Config --
+-- 	diagnostics = {
+-- 		darker = true, -- darker colors for diagnostic
+-- 		undercurl = true, -- use undercurl instead of underline for diagnostics
+-- 		background = true, -- use background color for virtual text
+-- 	},
+-- })
+--
+-- require("onedark").load()
 
-	code_style = {
-		comments = "italic",
-		keywords = "italic",
-		functions = "bold",
-		strings = "none",
-		variables = "none",
-	},
-	-- Plugins Config --
-	diagnostics = {
-		darker = true, -- darker colors for diagnostic
-		undercurl = true, -- use undercurl instead of underline for diagnostics
-		background = true, -- use background color for virtual text
-	},
-})
+local tokyonight = require("tokyonight")
+vim.cmd([[colorscheme tokyonight-night]])
 
-require("onedark").load()
+vim.keymap.set("n", "<leader>td", function()
+	vim.cmd([[colorscheme tokyonight-night]])
+end, opts)
+
+vim.keymap.set("n", "<leader>tl", function()
+	vim.cmd([[colorscheme tokyonight-day]])
+end, opts)
