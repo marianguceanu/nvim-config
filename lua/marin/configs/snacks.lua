3e2525ddc869bb881dc133fe22a433e562230dfc
Marian Guceanu <marian.guceanu@yahoo.com>
2025-04-07 17:17:07 +0300

    Added error lens

---
+++ lua/marin/configs/snacks.lua
@@ -0,0 +1,20 @@
+require("snacks").setup(
+---@type snacks.Config
+	{
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		explorer     = { enabled = true },
+		picker       = { enabled = true },
+		input        = { enabled = true },
+		bigfile      = { enabled = true },
+		dashboard    = { enabled = true },
+		indent       = { enabled = true },
+		notifier     = { enabled = true },
+		quickfile    = { enabled = true },
+		scope        = { enabled = true },
+		scroll       = { enabled = true },
+		statuscolumn = { enabled = true },
+		words        = { enabled = true },
+	}
+)
